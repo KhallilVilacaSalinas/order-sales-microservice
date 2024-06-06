8d4d4c55c5cc976041a91f1b3c85ad5f9c613f4e
vilaca <khallil.vilaca@gmail.com>
2024-06-05 21:45:18 -0300

    fix: remove migrations, create .env.example and config docker-compose

--- src/Order/Controller/orderController.ts
+++ src/Order/Controller/orderController.ts
@@ -3,6 +3,6 @@ import { Request, Response } from "express";
 export class OrderController {
 
     public orderCreateAction = async (req: Request, res: Response) => {
-        console.log('cheguei');
+        return res.json({'teste': 'chegou'})
     }
 }
